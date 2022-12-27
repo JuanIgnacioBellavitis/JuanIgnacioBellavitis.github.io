03c953cb5e7651dfe842bad094718187a281a639
JuanIMultiRadio <juan.bellavitis@gmail.com>
2022-12-27 09:24:05 -0300

    icons left sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,26 @@
- import './index.scss'
+import { Link, NavLink } from 'react-router-dom'; 
+import './index.scss'
+import LogoS from '../../assets/images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
- const Sidebar = () => <>Hello from sidebar</>
+ const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to="/">
+            <img src={LogoS} alt="logo"></img>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+            </NavLink>
+        </nav>
+    </div>
+ )
 
  export default Sidebar;
