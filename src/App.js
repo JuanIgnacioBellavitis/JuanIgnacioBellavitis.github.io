efc2f666a8a01717762a620b9f002bcee10599c9
JuanIMultiRadio <juan.bellavitis@gmail.com>
2022-12-27 08:55:12 -0300

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}></Route>
+    </Routes>
   );
 }
 
