64590389db863899f29078d7662c6ea79ad8c42f
JuanIMultiRadio <juan.bellavitis@gmail.com>
2022-12-27 08:21:44 -0300

    prueba Juani Multiradio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.scss';
 
 function App() {
   return (
-  <div></div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
